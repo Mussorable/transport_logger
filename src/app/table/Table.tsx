1a865d25efa7dffa0ce5e410642d26f94d496c7e
Mussorable <redduck5601@gmail.com>
2025-01-06 18:03:10 +0100

    Replaced inline forms components for adding routes and trucks.

--- src/app/table/Table.tsx
+++ src/app/table/Table.tsx
@@ -1,18 +1,24 @@
 import rightArrow from '../../assets/right-arrow.svg';
-import React, { ChangeEvent, FormEvent, ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import ModalWindow from "../../utils/ModalWindow.tsx";
 import AddTruck from "../../utils/modals/AddTruck.tsx";
 
 import moment from "moment/min/moment-with-locales";
+import AddRoute, { Route } from "./legend/AddRoute.tsx";
 
 const browserLanguage= navigator.language;
+
+export interface Legend {
+    number: string;
+    workLegend: Route[];
+}
 
 function Table() {
     moment.locale(browserLanguage);
 
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
     const [modalContent, setModalContent] = useState<ReactNode>(null);
-    const [legend, setLegend] = useState([
+    const [legend, setLegend] = useState<Legend[]>([
         {
             number: "5E7 1856",
             workLegend: [
@@ -83,70 +89,24 @@ function Table() {
             ]
         }
     ]);
-    const [newLegend, setNewLegend] = useState({
-        date: "",
-        deliveryTime: "",
-        to: "",
-        status: ""
-    });
-    const [editingLegends, setEditingLegends] = useState<string | null>(null);
+    const [editingTruck, setEditingTruck] = useState<string | null>(null);
 
-    const handleAddLegendClick = (truckNumber: string) => {
-        setEditingLegends(truckNumber);
+    const handleAddRoute = (route: Route, truckNumber: string) => {
+        setLegend((prevLegend) =>
+            prevLegend.map((truck) =>
+                truck.number === truckNumber
+                    ? { ...truck, workLegend: [...truck.workLegend, route] }
+                    : truck
+            )
+        );
+        setEditingTruck(truckNumber);
     };
-
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        if (name === 'deliveryTime') {
-            const formattedTime = moment(value, 'HH:mm', true).format('HH:mm');
-            setNewLegend((prev) => ({ ...prev, [name]: formattedTime }));
-        }
-        setNewLegend((prev) => ({ ...prev, [name]: value }));
-    };
-
-    const handleSubmitLegend = (event: FormEvent, truckNumber: string) => {
-        event.preventDefault();
-
-        const { date, deliveryTime, to, status } = newLegend;
-        if (to) {
-            const updatedLegend = {
-                ...newLegend,
-                date: date || moment().format("YYYY-MM-DD"),
-                deliveryTime: deliveryTime || moment().format("HH:mm"),
-                status: status || "LOADING"
-            };
-
-            setLegend((prevLegend) =>
-                prevLegend.map((truck) =>
-                    truck.number === truckNumber
-                        ? {...truck, workLegend: [...truck.workLegend, {...updatedLegend}]}
-                        : truck
-                )
-            );
-            console.log(updatedLegend);
-        }
-        setEditingLegends(null);
-        setNewLegend({
-            date: "",
-            deliveryTime: "",
-            to: "",
-            status: ""
-        });
-    };
-    const handleEscPress = (event: React.KeyboardEvent<HTMLFormElement>) => {
-        if (event.key === "Escape") {
-            setEditingLegends(null);
-            setNewLegend({
-                date: "",
-                deliveryTime: "",
-                to: "",
-                status: ""
-            });
-        }
+    const handleCancelAddRoute = () => {
+        setEditingTruck(null);
     };
 
     const handleAddTruckClick = () => {
-        setModalContent(<AddTruck />);
+        setModalContent(<AddTruck setLegend={setLegend} setIsModalVisible={setIsModalVisible} />);
         setIsModalVisible((prev) => !prev);
     };
 
@@ -157,7 +117,7 @@ function Table() {
                 return (
                     <div key={truck.number} className="flex bg-gray-900 font-semibold border-b border-gray-400">
                         <div className="text-white px-2 bg-gray-700 w-32">
-                            <p>{truck.number}</p>
+                            <p>{truck.number.toUpperCase()}</p>
                         </div>
                         <div className="flex-1 flex">
                             {truck.workLegend && truck.workLegend.map((route, index) => {
@@ -165,7 +125,7 @@ function Table() {
                                     <div key={index} className="flex">
                                         <div
                                              className={ `text-sm font-light border-x px-2 ${ route.status === 'DONE' ? 'bg-gray-400' : route.status === 'LOADING' ? 'bg-green-300' : 'bg-blue-300' }` }>
-                                            <p>{ moment(route.date).format("DD.MM dddd") }</p>
+                                            <p>{ moment(route.date).format("DD.MM ddd").toUpperCase() }</p>
                                             <p>{ `${route.to[0].toUpperCase()}${route.to.slice(1).toLowerCase()}` }</p>
                                             <p>{ route.deliveryTime }</p>
                                         </div>
@@ -175,26 +135,17 @@ function Table() {
                                 )
                             }) }
                             <div className="text-sm font-light border-x relative">
-                                { editingLegends === truck.number ?
+                                { editingTruck === truck.number ?
                                     (
-                                        <form
-                                            onSubmit={(e) => handleSubmitLegend(e, truck.number)}
-                                            onKeyDown={e => handleEscPress(e)}
-                                            className="flex flex-col">
-                                            <input className="w-24" required={true} name="date" onChange={handleInputChange} value={newLegend.date || moment().format("YYYY-MM-DD")} type={"date"}/>
-                                            <input className="w-24" required={true} name="to" placeholder="Place" onChange={handleInputChange} value={newLegend.to} type="text"/>
-                                            <input className="w-24" required={true} name="deliveryTime" placeholder="HH:MM" onChange={handleInputChange} value={newLegend.deliveryTime || moment().format("HH:mm")} type="text"/>
-                                            <select className={ `w-24 ${newLegend.status === 'LOADING' ? 'bg-green-300' : newLegend.status === 'UNLOADING' ? 'bg-blue-300' : 'bg-red-300'}` } required={true} name="status" id="" value={newLegend.status} onChange={handleInputChange}>
-                                                <option value=""></option>
-                                                <option className="bg-green-300" value="LOADING">Loading</option>
-                                                <option className="bg-blue-300" value="UNLOADING">Unloading</option>
-                                            </select>
-                                            <button type="submit" className="hidden">submit</button>
-                                        </form>
+                                        <AddRoute
+                                            truckNumber={truck.number}
+                                            onRouteAdded={handleAddRoute}
+                                            onCancel={handleCancelAddRoute}
+                                        />
                                     ) :
                                     (
                                     <button className="bg-cyan-200 w-full h-full px-5 font-semibold"
-                                            onClick={ () => handleAddLegendClick(truck.number) }>Add</button>
+                                            onClick={ () => setEditingTruck(truck.number) }>Add</button>
                                     )
                                 }
                             </div>
--- src/utils/modals/AddTruck.tsx
+++ src/utils/modals/AddTruck.tsx
@@ -1,10 +1,36 @@
-function AddTruck() {
+import { FormEvent, useState } from "react";
+import { Legend } from "../../app/table/Table.tsx";
+
+interface AddTruckProps {
+    setLegend: (updater: (prevLegend: Legend[]) => Legend[]) => void;
+    setIsModalVisible: (value: boolean) => void;
+}
+
+function AddTruck({ setLegend, setIsModalVisible }: AddTruckProps) {
+    const [truckNumber, setTruckNumber] = useState("");
+
+    const handleLegendSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        setLegend((prevLegend) => {
+            return [
+                ...prevLegend,
+                {
+                    number: truckNumber,
+                    workLegend: []
+                }
+            ]
+        });
+
+        setIsModalVisible(false);
+    };
+
     return (
-        <form action="" method="POST">
+        <form onSubmit={handleLegendSubmit} action="" method="POST">
             <h2 className="text-xl font-semibold text-center">Add Truck</h2>
             <div className="my-4 flex gap-4">
                 <label htmlFor="">Truck number</label>
-                <input type="text"/>
+                <input value={truckNumber} onChange={e => setTruckNumber(e.currentTarget.value)} type="text"/>
             </div>
             <button type="submit" className="px-4 bg-blue-400 text-white block w-full">Add</button>
         </form>
